392382784954b1df3feedf85c25a7fbd21ada95c
ram4297 <godissairam@gmail.com>
2021-07-03 23:23:41 +0530

    Removed secret key

--- to_do_list_featured/settings.py
+++ to_do_list_featured/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7=a)^x5@%_qy(-oy$^^sjs82-)9x56xydn2d5qf7h_(tg(pb3r'
+SECRET_KEY = 'dumb-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
